615fe57c0269caccead06a4132ccce726ce407a9
Daria Levchenko <dashentus96@mail.ru>
2018-08-05 20:11:08 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gcc `pkg-config --cflags gtk+-3.0` -o MAFIA_client mafia_client.c Mafia.c gtk.c `pkg-config --libs gtk+-3.0` -std=c99
-gcc `pkg-config --cflags gtk+-3.0` -o MAFIA_server mafia_server.c `pkg-config --libs gtk+-3.0` -std=c99 -lrt
+gcc `pkg-config --cflags gtk+-3.0` -o MAFIA_server mafia_server.c `pkg-config --libs gtk+-3.0` -std=c99
